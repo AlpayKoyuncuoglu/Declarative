47189f8ee1b7f44edf67bd2f0afeb623b3c936c3
AlpayKoyuncuoglu <41324656+AlpayKoyuncuoglu@users.noreply.github.com>
2022-02-18 16:12:13 +0300

    sec

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { IPost } from '../models/IPost';
-import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -12,19 +11,7 @@ export class PostService {
   getPosts() {
     return this.http.get<{ [id: string]: IPost }>(
       `https://angular-rxjs-posts-2e8b5-default-rtdb.firebaseio.com/posts.json`
-    ).pipe(map(posts => {
-
-      let postsData: IPost[] = [];
-      console.log("posts");
-      console.log(posts);
-
-      for (let id in posts) {
-        postsData.push({...posts[id],id })
-      }
-      console.log("service");
-      console.log(postsData);
-      return postsData;
-    }));
-
+    )
+      console.log("asd")
   }
 }
