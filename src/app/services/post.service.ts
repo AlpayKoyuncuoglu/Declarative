fe3f2c8e2f51be5d5282473c5f5be561611c0109
akoyuncuoglu <akoyuncuoglu@PC-KOYUNCUOGLU>
2022-02-18 16:39:41 +0300

    2nd

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -10,21 +10,15 @@ export class PostService {
   constructor(private http: HttpClient) { }
 
   getPosts() {
-    // deneme
-console.log("asdasdasdasd");
     return this.http.get<{ [id: string]: IPost }>(
       `https://angular-rxjs-posts-2e8b5-default-rtdb.firebaseio.com/posts.json`
     ).pipe(map(posts => {
 
       let postsData: IPost[] = [];
-      console.log("posssssssssssts");
-      console.log(posts);
 
       for (let id in posts) {
         postsData.push({...posts[id],id })
       }
-      console.log("service");
-      console.log(postsData);
       return postsData;
     }));
 
